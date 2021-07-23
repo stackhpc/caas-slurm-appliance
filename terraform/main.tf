b0c0d3ce3d543e733172de1fe54b00e13125d7e5
Matt Pryor <matt@stackhpc.com>
2021-07-23 21:08:15 +0100

    Store Terraform state in Consul

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,15 @@
 terraform {
   required_version = ">= 0.14"
+
+  # We need the OpenStack provider
   required_providers {
     openstack = {
       source = "terraform-provider-openstack/openstack"
     }
   }
+
+  # The Terraform state lives in Consul
+  backend "consul" { gzip = true }
 }
 
 #####
