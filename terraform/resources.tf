65acad81dfb7efaf101ee1cfbb983c31fca58470
Matt Pryor <matt@stackhpc.com>
2021-07-26 09:55:21 +0100

    Allow Terraform backend to be changed

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 0.14"
-
-  # We need the OpenStack provider
-  required_providers {
-    openstack = {
-      source = "terraform-provider-openstack/openstack"
-    }
-  }
-
-  # The Terraform state lives in Consul
-  backend "consul" { gzip = true }
-}
-
 #####
 ##### Security groups for the cluster
 #####
